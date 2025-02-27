cebad92de6f4543dc0e18a6a288ba22b46a247f5
Jairo Santos <jairojeffersont@gmail.com>
2025-02-27 12:14:22 -0300

    v1

--- src/Views/pessoa/imprimir-pessoa.php
+++ src/Views/pessoa/imprimir-pessoa.php
@@ -24,45 +24,22 @@ if ($buscaPessoa['status'] == 'not_found' || is_integer($pessoaGet) || $buscaPes
 ?>
 
 <style>
-    body {
-        background-image: url(public/img/print_bg.jpeg);
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
-    }
-
     @media print {
 
+        body {
+            background-color: rgb(255, 255, 255);
+        }
+
         @page {
-            margin: 0;
+            margin: 15mm;
             size: A4 portrait;
         }
 
-        html,
         body {
-            height: 100%;
-            width: 100%;
-            margin: 0;
-            padding: 0;
+            background-color: rgb(255, 255, 255);
         }
 
-        body {
-            background-image: url(public/img/print_bg.jpeg);
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-        }
 
-        header,
-        footer {
-            display: none !important;
-        }
-
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
     }
 </style>
 
@@ -77,79 +54,44 @@ if ($buscaPessoa['status'] == 'not_found' || is_integer($pessoaGet) || $buscaPes
 </script>
 
 <div class="container-fluid p-2">
-    <div class="row mt-4">
-        <div class="col-12">
-            <div class="card" style="background: none; border: none;">
-                <div class="card-body text-center" style="background: none;">
-                    <img src="public/img/brasaooficialcolorido.png" class="img-fluid mb-2" style="width: 150px;" />
-                    <h5 class="card-title mb-2">Gabinete <?php echo $buscaGab['dados']['gabinete_nome'] ?></h5>
-                    <p class="card-text" style="font-size: 1.4em;">Ficha cadastral </p>
-                </div>
-            </div>
+
+    <div class="row">
+        <div class="col-12 text-center mb-1">
+            <h5>Gabinete <?php echo $buscaGab['dados']['gabinete_nome'] ?></h5>
         </div>
-    </div>
-
-
-    <div class="row mt-3 mb-2 d-flex justify-content-center">
-        <div class="col-10">
-            <div class="card" style="background: none; border: none;">
-                <div class="card-body" style="background: none;">
-                    <h6 class="card-title">Informações Pessoais</h6>
-                    <hr>
-                    <p class="card-text mb-1">Nome: <?php echo $buscaPessoa['dados']['pessoa_nome'] ?></p>
-                    <p class="card-text mb-1">Aniversário: <?php echo $buscaPessoa['dados']['pessoa_aniversario'] ?></p>
-                    <p class="card-text mb-1">Profissão: <?php echo $buscaPessoa['dados']['pessoas_profissoes_nome'] ?></p>
-                    <p class="card-text mb-1">Órgão/entidade: <?php echo $buscaPessoa['dados']['orgao_nome'] ?></p>
-                    <p class="card-text mb-1">Cargo: <?php echo $buscaPessoa['dados']['pessoa_cargo'] ?></p>
-                </div>
-            </div>
+        <div class="col-12 text-center mb-5">
+            <h6>Ficha cadastral</h6>
         </div>
-    </div>
-
-
-    <div class="row d-flex mb-2 justify-content-center">
-        <div class="col-10">
-            <div class="card" style="background: none; border: none;">
-                <div class="card-body" style="background: none;">
-                    <h6 class="card-title">Informações de contato</h6>
-                    <hr>
-                    <p class="card-text mb-1">Telefone: <?php echo $buscaPessoa['dados']['pessoa_telefone'] ?></p>
-                    <p class="card-text mb-1">Endereço: <?php echo $buscaPessoa['dados']['pessoa_endereco'] ?></p>
-                    <p class="card-text mb-1">Bairro: <?php echo $buscaPessoa['dados']['pessoa_bairro'] ?></p>
-                    <p class="card-text mb-1">Município: <?php echo $buscaPessoa['dados']['pessoa_municipio'] . '/' . $buscaPessoa['dados']['pessoa_estado'] ?></p>
-                    <p class="card-text mb-1">CEP: <?php echo $buscaPessoa['dados']['pessoa_cep'] ?></p>
-                </div>
-            </div>
+        <div class="col-12 p-2 mb-3">
+            <h6 class="card-title">Informações Pessoais</h6>
+            <hr>
+            <p class="card-text mb-1">Nome: <?php echo $buscaPessoa['dados']['pessoa_nome'] ?></p>
+            <p class="card-text mb-1">Aniversário: <?php echo date('d/m', strtotime($buscaPessoa['dados']['pessoa_aniversario'])) ?></p>
+            <p class="card-text mb-1">Profissão: <?php echo $buscaPessoa['dados']['pessoas_profissoes_nome'] ?></p>
+            <p class="card-text mb-1">Órgão/entidade: <?php echo $buscaPessoa['dados']['orgao_nome'] ?></p>
+            <p class="card-text mb-1">Cargo: <?php echo !empty($buscaPessoa['dados']['pessoa_cargo']) ? $buscaPessoa['dados']['pessoa_cargo'] : 'Não informado' ?></p>
+            <p class="card-text mb-1">Sexo: <?php echo !empty($buscaPessoa['dados']['pessoa_sexo']) ? $buscaPessoa['dados']['pessoa_sexo'] : 'Não informado' ?></p>
+            <p class="card-text mb-1">Tipo: <?php echo !empty($buscaPessoa['dados']['pessoa_tipo_nome']) ? $buscaPessoa['dados']['pessoa_tipo_nome'] : 'Não informado' ?></p>
         </div>
-    </div>
-
-    <div class="row d-flex justify-content-center mb-2">
-        <div class="col-10">
-            <div class="card" style="background: none; border: none;">
-                <div class="card-body" style="background: none;">
-                    <h6 class="card-title">Redes sociais</h6>
-                    <hr>
-                    <p class="card-text mb-1">Instagram: <?php echo $buscaPessoa['dados']['pessoa_instagram'] ?></p>
-                    <p class="card-text mb-1">Facebook: <?php echo $buscaPessoa['dados']['pessoa_facebook'] ?></p>
-                    <p class="card-text mb-1">X (Twitter): <?php echo $buscaPessoa['dados']['pessoa_x'] ?></p>
-                </div>
-            </div>
+        <div class="col-12 p-2">
+            <h6 class="card-title">Informações de contato</h6>
+            <hr>
+            <p class="card-text mb-1">Telefone: <?php echo !empty($buscaPessoa['dados']['pessoa_telefone']) ? $buscaPessoa['dados']['pessoa_telefone'] : 'Não informado' ?></p>
+            <p class="card-text mb-1">Endereço: <?php echo !empty($buscaPessoa['dados']['pessoa_endereco']) ? $buscaPessoa['dados']['pessoa_endereco'] : 'Não informado' ?></p>
+            <p class="card-text mb-1">Bairro: <?php echo !empty($buscaPessoa['dados']['pessoa_bairro']) ? $buscaPessoa['dados']['pessoa_bairro'] : 'Não informado' ?>
+            <p class="card-text mb-1">Município: <?php echo !empty($buscaPessoa['dados']['pessoa_municipio']) ? $buscaPessoa['dados']['pessoa_municipio'] : 'Não informado' ?></p>
+            <p class="card-text mb-1">CEP: <?php echo !empty($buscaPessoa['dados']['pessoa_cep']) ? $buscaPessoa['dados']['pessoa_cep'] : 'Não informado' ?></p>
+            </p>
         </div>
-    </div>
-
-    <div class="row d-flex justify-content-center">
-        <div class="col-10">
-            <div class="card" style="background: none; border: none;">
-                <div class="card-body" style="background: none;">
-                    <h6 class="card-title">Informações adicionais</h6>
-                    <hr>
-                    <p class="card-text mb-1"> <?php echo $buscaPessoa['dados']['pessoa_informacoes'] == null ? 'Sem informações adicionais' : $buscaPessoa['dados']['pessoa_informacoes'] ?>
-                    </p>
-                </div>
-            </div>
+        <div class="col-12 p-2">
+            <h6 class="card-title">Redes sociais</h6>
+            <hr>
+            <p class="card-text mb-1">Instagram: <?php echo !empty($buscaPessoa['dados']['pessoa_instagram']) ? $buscaPessoa['dados']['pessoa_instagram'] : 'Não informado' ?></p>
+            <p class="card-text mb-1">Facebook: <?php echo !empty($buscaPessoa['dados']['pessoa_facebook']) ? $buscaPessoa['dados']['pessoa_facebook'] : 'Não informado' ?>
+            <p class="card-text mb-1">X (Twitter): <?php echo !empty($buscaPessoa['dados']['pessoa_x']) ? $buscaPessoa['dados']['pessoa_x'] : 'Não informado' ?></p>
         </div>
-    </div>
-
-
-
-</div>
+        <div class="col-12 p-2">
+            <h6 class="card-title">Informações adicionais</h6>
+            <hr>
+            <p class="card-text mb-1"> <?php echo $buscaPessoa['dados']['pessoa_informacoes'] == null ? 'Sem informações adicionais' : $buscaPessoa['dados']['pessoa_informacoes'] ?></p>
+        </div>
