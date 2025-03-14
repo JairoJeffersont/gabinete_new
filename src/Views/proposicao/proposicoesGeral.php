c182f9ef697790d31d10c1828f304ca0a09a76f6
Jairo Santos <jairojeffersont@gmail.com>
2025-03-14 12:45:02 -0300

    v1

---
+++ src/Views/proposicao/proposicoesGeral.php
@@ -0,0 +1,6 @@
+<div class="card mb-2">
+    <div class="card-header bg-primary text-white px-2 py-1 card_descricao_bg"><i class="bi bi-newspaper"></i> Proposições | <?php echo $tipoGabinete['dados']['gabinete_tipo_nome'] ?> | <?php echo $gabinete['dados']['gabinete_nome'] ?></div>
+    <div class="card-body card_descricao_body p-2">
+        <p class="card-text mb-0">Em desenvolvimento</p>
+    </div>
+</div>
